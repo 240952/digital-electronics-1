18018896811df026801ef73e79c7d49593a4ac64
Keshaay <95315728+Keshaay@users.noreply.github.com>
2023-05-01 19:13:45 +0200

    Update UART_RX.vhd

--- project_UART/project_UART.srcs/sources_1/new/UART_RX.vhd
+++ project_UART/project_UART.srcs/sources_1/new/UART_RX.vhd
@@ -55,81 +55,83 @@ begin
 ---------------------------------------
     --Vzorkovani serieoveho vstupu
 --------------------------------------- 
-    IN_sample : process(Clk)
+ IN_sample : process(Clk)
         begin
             if Clk'event and Clk ='1' then
                 H_data <= rx_serial;
                 A_data <= H_data;
             end if;    
     end process;
-    UART_receiver : process(Clk)
+  UART_receiver : process(Clk)
         begin
             if Clk'event and Clk = '1' then
                 case r_SM_Main is 
-                    ----------------------------------------------------------------
-                    -- Vstupni stav
-                    ----------------------------------------------------------------
+----------------------------------------------------------------
+-- Vstupni stav
+----------------------------------------------------------------
                     
-                    -- s_data_valid                    nula do platnych dat
-                    -- s_indexBitA                     nula do aktualniho indexu bitu
-                    -- s_StartBit_rx;                  do stavu start
-                    -- A_data = '0'                    aktualni hodnota signalu
-                    -- s_ClkCount <=0;                 reset clkcount
-                    -- s_ClkCount <= s_ClkCount +1;    inkrementace count
-                    -- s_bytes(s_indexBitA) <= A_data; ulozeni prijeteho bitu
-                    -- if s_indexBitA<7                zde kontroluju,zda jsem prijal vsechny bity
-                    -- s_data_valid <= '1';            oznacim si, ze znam nove data
-                    -----------------------------------------------------------------
-                    when s_idle => 
-                        s_data_valid<=  '0'; 
-                        s_ClkCount  <=   0;  
-                        s_indexBitA <=   0;  
+-- |84| s_data_valid                    nula do platnych dat
+-- |86| s_indexBitA                     nula do aktualniho indexu bitu
+-- |93| s_StartBit_rx;                  do stavu start
+-- |95| A_data = '0'                    aktualni hodnota signalu
+-- |96| s_ClkCount <=0;                 reset clkcount
+-----------------------------------------------------------------
+    when s_idle => 
+          s_data_valid<=  '0'; 
+          s_ClkCount  <=   0;  
+          s_indexBitA <=   0;  
                         
-                        if A_data = '0' then 
-                            r_SM_Main <= s_StartBit_rx; 
-                        else
-                             r_SM_Main <= s_idle; 
-                        end if;        
-                    when s_StartBit_rx =>
-                        if s_ClkCount = (Clk_per_bit-1)/2 then 
-                            if A_data = '0' then 
-                                s_ClkCount <=0; 
-                                r_SM_Main <= s_DataBits_rx; 
-                             else
-                                r_SM_Main <= s_idle;   
-                            end if;
-                            s_ClkCount <= s_ClkCount +1; 
-                            r_SM_Main <= s_StartBit_rx;                            
-                        end if;  
-                   when  s_DataBits_rx => 
-                        if s_ClkCount < Clk_per_bit-1 then 
-                            s_ClkCount <= s_ClkCount +1;
-                            r_SM_Main <= s_DataBits_rx; 
-                        else
-                            s_ClkCount <= 0;
-                            s_bytes(s_indexBitA) <= A_data;  
-                            if s_indexBitA<7 then 
-                                s_indexBitA <= s_indexBitA+1;
-                                r_SM_Main <=s_DataBits_rx;
-                            else
-                                s_indexBitA <=0;
-                                r_SM_Main <=s_StopBit_rx; 
-                            end if;  
-                        end if;    
-                   when s_StopBit_rx => 
-                              if s_ClkCount < Clk_per_bit-1 then
-                                    s_ClkCount <= s_ClkCOunt + 1;
-                                    r_SM_Main <= s_StopBit_rx;
-                              else  
-                                    s_data_valid <= '1'; 
-                                    s_ClkCount <= 0;
-                                    r_SM_Main <=s_clear;
-                              end if;
-                   when  s_clear =>
-                        r_SM_Main <= s_idle;
-                        s_data_valid <='0';
-                   when others =>
-                        r_SM_Main <= s_idle;                                                    
+     if A_data = '0' then 
+          r_SM_Main <= s_StartBit_rx; 
+     else
+           r_SM_Main <= s_idle; 
+     end if;        
+       when s_StartBit_rx =>
+          if s_ClkCount = (Clk_per_bit-1)/2 then 
+          if A_data = '0' then 
+             s_ClkCount <=0; 
+             r_SM_Main <= s_DataBits_rx; 
+      else
+             r_SM_Main <= s_idle;   
+      end if;
+             s_ClkCount <= s_ClkCount +1; 
+             r_SM_Main <= s_StartBit_rx;                            
+      end if;  
+      when   s_DataBits_rx => 
+      if s_ClkCount < Clk_per_bit-1 then 
+             s_ClkCount <= s_ClkCount +1;
+             r_SM_Main <= s_DataBits_rx; 
+      else
+             s_ClkCount <= 0;
+             s_bytes(s_indexBitA) <= A_data;  
+      if s_indexBitA<7 then 
+             s_indexBitA <= s_indexBitA+1;
+             r_SM_Main <=s_DataBits_rx;
+      else
+             s_indexBitA <=0;
+             r_SM_Main <=s_StopBit_rx;
+----------------------------------------------------------------------------
+-- |101|s_ClkCount <= s_ClkCount +1;    inkrementace count
+-- |110|s_bytes(s_indexBitA) <= A_data; ulozeni prijeteho bitu
+-- |111|if s_indexBitA<7                zde kontroluju,zda jsem prijal vsechny bity
+-- |124|s_data_valid <= '1';            oznacim si, ze znam nove data
+-----------------------------------------------------------------------------
+      end if;  
+      end if;    
+      when s_StopBit_rx => 
+      if s_ClkCount < Clk_per_bit-1 then
+               s_ClkCount <= s_ClkCOunt + 1;
+               r_SM_Main <= s_StopBit_rx;
+       else  
+               s_data_valid <= '1'; 
+               s_ClkCount <= 0;
+               r_SM_Main <=s_clear;
+      end if;
+        when  s_clear =>
+                r_SM_Main <= s_idle;
+                s_data_valid <='0';
+        when others =>
+                 r_SM_Main <= s_idle;                                                    
                 end case; 
             end if;
     end process;
